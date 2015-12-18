da53957d41f8feec73c6afc75b0086d5b9408545
FranPrimo <frapriru@gmail.com>
2015-12-18 16:43:21 +0100

    No detiende el hilo

---
+++ app/src/main/java/com/example/franprimo/syp_p2/TareaMuyCostosa.java
@@ -0,0 +1,36 @@
+package com.example.franprimo.syp_p2;
+
+import android.util.Log;
+
+/**
+ * Created by FranPrimo on 18/12/15.
+ */
+public class TareaMuyCostosa extends Thread implements Runnable {
+    private String TAG = "Practica 2 SYP: ";
+    private volatile boolean threadStoped = false;
+
+    public void stopThread(){
+        Log.i(TAG, "Aqui llega");
+        threadStoped = true;
+    }
+
+    @Override
+    public void run(){
+        Log.i(TAG, "Tarea muy costosa iniciada");
+        while(true){
+            if(threadStoped){
+                Log.i(TAG, "Y aqui tambien llega");
+                break;
+            }
+            try{
+                for(int i=0; i<100; i++) {
+                    Log.i(TAG, "Tarea muy costosa en marcha");
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        Log.i(TAG, "Tarea muy costosa finalizada");
+    }
+}
