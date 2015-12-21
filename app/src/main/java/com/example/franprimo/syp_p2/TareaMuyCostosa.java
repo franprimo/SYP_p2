c1f36999773c0cf42a94fcd1cc13b1e02cac93f4
FranPrimo <frapriru@gmail.com>
2015-12-21 17:44:03 +0100

    variacion dentro del run

--- app/src/main/java/com/example/franprimo/syp_p2/TareaMuyCostosa.java
+++ app/src/main/java/com/example/franprimo/syp_p2/TareaMuyCostosa.java
@@ -5,12 +5,11 @@ import android.util.Log;
 /**
  * Created by FranPrimo on 18/12/15.
  */
-public class TareaMuyCostosa extends Thread implements Runnable {
+public class TareaMuyCostosa extends Thread {
     private String TAG = "Practica 2 SYP: ";
     private volatile boolean threadStoped = false;
 
     public void stopThread(){
-        Log.i(TAG, "Aqui llega");
         threadStoped = true;
     }
 
@@ -19,13 +18,9 @@ public class TareaMuyCostosa extends Thread implements Runnable {
         Log.i(TAG, "Tarea muy costosa iniciada");
         try{
             for(int i=0; i<100; i++) {
-                if(threadStoped) {
-                    Log.i(TAG, "Y aqui tambien llega");
-                    break;
-                }else{
-                    Log.i(TAG, "Tarea muy costosa en marcha");
-                    Thread.sleep(1000);
-                }
+                if(threadStoped) break;
+                Log.i(TAG, "Tarea muy costosa en marcha");
+                Thread.sleep(1000);
             }
         }catch (InterruptedException e) {
             Thread.currentThread().interrupt();
